6f5ae17409cc31adcbd3738598d31a4d5259ee66
d.szablinski <d.szablinski@mob4809>
2015-01-16 21:22:28 +0100

    fixed locale

--- src/main/java/com/insightfullogic/java8/answers/chapter2/Question2.java
+++ src/main/java/com/insightfullogic/java8/answers/chapter2/Question2.java
@@ -1,12 +1,10 @@
 package com.insightfullogic.java8.answers.chapter2;
 
 import java.text.SimpleDateFormat;
-import java.text.SimpleDateFormat;
-
-import static java.lang.ThreadLocal.withInitial;
+import java.util.Locale;
 
 public class Question2 {
 
-     public final static ThreadLocal<SimpleDateFormat> formatter = ThreadLocal.withInitial(() -> new SimpleDateFormat("dd-MMM-yyyy") );
+     public final static ThreadLocal<SimpleDateFormat> formatter = ThreadLocal.withInitial(() -> new SimpleDateFormat("dd-MMM-yyyy", Locale.US) );
 
 }
